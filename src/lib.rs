b0d0871b1fa89ff63a9715d1a33b0dca11fc1385
Philipp Krüger <philipp.krueger1@gmail.com>
2024-10-21 17:48:45 +0200

    :rocket: Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod config;
+pub mod doctor;
+pub mod logging;
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::{net::SocketAddr, path::PathBuf, time::Duration};
+
+use anyhow::Result;
+use clap::Parser;
+use iroh_doctor::{config::NodeConfig, doctor::Commands};
+
+/// iroh-doctor is a tool for diagnosing network issues with iroh-net.
+///
+/// For more information, visit: <https://iroh.computer/docs>.
+#[derive(Parser, Debug, Clone)]
+#[clap(version, verbatim_doc_comment)]
+pub(crate) struct Cli {
+    #[clap(subcommand)]
+    pub(crate) command: Commands,
+
+    /// Path to the configuration file, see https://iroh.computer/docs/reference/config.
+    #[clap(long)]
+    pub(crate) config: Option<PathBuf>,
+
+    /// Address to serve metrics on. Disabled by default.
+    #[clap(long)]
+    pub(crate) metrics_addr: Option<SocketAddr>,
+
+    /// Write metrics in CSV format at 100ms intervals. Disabled by default.
+    #[clap(long)]
+    pub(crate) metrics_dump_path: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .thread_name("main-runtime")
+        .worker_threads(2)
+        .enable_all()
+        .build()?;
+    rt.block_on(main_impl())?;
+    // give the runtime some time to finish, but do not wait indefinitely.
+    // there are cases where the a runtime thread is blocked doing io.
+    // e.g. reading from stdin.
+    rt.shutdown_timeout(Duration::from_millis(500));
+    Ok(())
+}
+
+async fn main_impl() -> Result<()> {
+    let cli = Cli::parse();
+    iroh::metrics::try_init_metrics_collection()?;
+    let config = NodeConfig::load(cli.config.as_deref()).await?;
+    iroh_doctor::doctor::run(cli.command, &config).await
+}
